23cdd102ae41a810dcbbb044802f7becdc786d8e
Ing. Alejandro Soto Treviño <soto.its@gmail.com>
2024-02-27 19:38:39 -0600

    27 Feb

--- lib/place_widget.dart
+++ lib/place_widget.dart
@@ -9,6 +9,7 @@ class PlaceWidget extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
 
     Widget imageSection = Image.asset('assets/img/place.jpeg', height: height * .30, width:width , fit: BoxFit.cover);
+    Widget imageSectionLandscape = Image.asset('assets/img/place.jpeg', height: height , width:width * 0.45 , fit: BoxFit.cover);
     Widget titleSection = Container(
       padding: EdgeInsets.all(32),
       child: Row(
@@ -38,13 +39,44 @@ class PlaceWidget extends StatelessWidget {
         _buildColumn("SHARE", Icons.share, Colors.blue),
       ],
     );
+    Widget descriptionSection = Padding(
+      padding: EdgeInsets.all(32),
+      child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus quam, commodo non felis id, gravida euismod odio. Morbi arcu felis, maximus nec ex accumsan, luctus facilisis diam. Nunc ullamcorper augue id magna dictum, eget pellentesque orci accumsan. Morbi est turpis, suscipit ut luctus eu, convallis sit amet neque. Sed auctor risus felis, vel dignissim nisl vestibulum quis. Mauris mauris ante, gravida eget arcu ac, tincidunt finibus enim. Vestibulum ac lacus nunc. In vestibulum dignissim augue, in venenatis elit tincidunt ut. Nam nunc diam, laoreet eu tortor id, hendrerit varius neque. Vestibulum suscipit mi lobortis porttitor tristique. Nulla egestas pretium venenatis. Phasellus quis enim orci."
+          "Vivamus vitae mauris suscipit, fermentum dui vitae, venenatis est. Cras faucibus interdum metus vel pellentesque. Vivamus ut semper purus, a auctor sapien. Fusce eget felis tellus. Curabitur ornare interdum arcu, quis tempor tortor. Proin dapibus turpis ac libero aliquet, at ornare nunc porttitor. Pellentesque dui mauris, volutpat molestie commodo sit amet, pulvinar eget ipsum. Aenean tristique, orci eget pellentesque sagittis, justo elit congue eros, nec consectetur nisi ante eu nisi. In egestas varius odio, ac vestibulum lectus tincidunt quis."
+          "Sed ut ultricies libero. Praesent semper libero neque, sed convallis tellus molestie vel. Maecenas aliquet ipsum vel felis varius, at convallis nunc pharetra. Quisque in ornare est, non condimentum turpis. Phasellus eu pellentesque leo. Maecenas sagittis, orci at suscipit gravida, ex dolor pellentesque nulla, in mollis nunc tellus sed ligula. Mauris sodales quam in nunc consectetur, et scelerisque libero dictum. Maecenas dui nibh, sagittis ut velit eu, accumsan vulputate ante. Donec mollis orci ante, non congue ante suscipit sed. Donec vitae magna sem. Quisque vel pretium enim.",
+       textAlign: TextAlign.justify,
+      ),
+    );
     return Scaffold(
-      body: Column(
-        children: [
-          imageSection,
-          titleSection,
-          buttonsSection
-        ],
+      body: OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) {
+          return orientation==Orientation.portrait ? SingleChildScrollView(
+            child: Column(
+              children: [
+                imageSection,
+                titleSection,
+                buttonsSection,
+                descriptionSection
+              ],
+            ),
+          ) : 
+          Row(
+            children: [
+              imageSectionLandscape,
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      titleSection,
+                      buttonsSection,
+                      SafeArea(child:descriptionSection) //SafeArea evita colisionar con notchs
+                    ],
+                  ),
+                ),
+              )
+            ],
+          );
+        }
       ),
     );
   }
