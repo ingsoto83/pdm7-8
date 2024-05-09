37ebe80a2852ff2154dd3a9c3b0d144e709fc9e6
Ing. Alejandro Soto Treviño <soto.its@gmail.com>
2024-05-08 20:02:06 -0600

    8 mayo

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,9 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:pdm7/list_widget.dart';
+
+
 
 class LoginPage extends StatefulWidget {
   LoginPage({super.key});
@@ -11,9 +13,33 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   final _formKey = GlobalKey<FormState>();
   bool visibility = false;
+  bool loading = false;
+  String user ="";
+  String pass ="";
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    if(_auth.currentUser!=null){
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+              builder: (context) {
+                return const ListWidget();
+              }
+          ), (route) => false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -70,7 +96,9 @@ class _LoginPageState extends State<LoginPage> {
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
                         child: TextFormField(
-                          decoration: InputDecoration(
+                          validator: (value)=> value!.isEmpty ? "Escribe tu usuario...!" : null,
+                          onSaved: (value)=> user = value??'',
+                          decoration: const InputDecoration(
                             prefixIcon: Icon(Icons.email, color: Colors.purple,),
                             labelText: "Usuario",
                             labelStyle: TextStyle(color: Colors.purple),
@@ -90,6 +118,16 @@ class _LoginPageState extends State<LoginPage> {
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
                         child: TextFormField(
+                          validator: (value){
+                            if(value!.isEmpty){
+                              return "Escribe tu contraseña...!";
+                            }else if(value.length < 6){
+                              return "Debe ser de al menos 6 caracteres...!";
+                            }else{
+                              return null;
+                            }
+                          },
+                          onSaved: (value)=> pass = value??'',
                           obscureText: visibility ? false : true,
                           decoration: InputDecoration(
                             prefixIcon: Icon(Icons.email, color: Colors.purple,),
@@ -116,6 +154,18 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                       ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+                        child: ElevatedButton(
+                          onPressed: (){
+                            if(_formKey.currentState!.validate()){
+                              _formKey.currentState!.save();
+                              login();
+                            }
+                          },
+                          child: loading ? Row(children: [CircularProgressIndicator(), SizedBox(width: 10,), Text("Cargando...")],) : Text("Iniciar Sesión")
+                        )
+                      )
                     ],
                   ),
                 ),
@@ -133,9 +183,45 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   login() async {
-    UserCredential result = await _auth.signInWithEmailAndPassword(
-        email: 'soto@its.mx', password: 'soto123');
-    setState(() {});
-    print(result);
+   setState(() {
+     loading=true;
+   });
+   try{
+    await _auth.signInWithEmailAndPassword(email: user, password: pass);
+   }catch(e){
+     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+       backgroundColor: Colors.red,
+       duration: Duration(seconds: 4),
+       content: Row(
+         children: [
+           Icon(Icons.dangerous, color: Colors.white),
+           SizedBox(width: 16,),
+           Expanded(
+             child: Text(e.toString()),
+           )
+         ],
+       ),
+     ));
+   }finally{
+     setState(() {
+       loading=false;
+     });
+     if(_auth.currentUser!=null){
+       /* Si unicamente hemos abierto un solo widget
+       Navigator.pushReplacement(
+           context,
+           MaterialPageRoute(
+               builder:(context)=>ListWidget(),
+           )
+       );*/
+       Navigator.pushAndRemoveUntil(
+           context,
+           MaterialPageRoute(
+               builder: (context) {
+                 return const ListWidget();
+               }
+           ), (route) => false);
+     }
+   }
   }
 }
